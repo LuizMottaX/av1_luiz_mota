8e354d4f82574a7fdd4ee32a757221bf44dceba5
Luiz Mota <118900569+LuizMottaX@users.noreply.github.com>
2025-04-04 19:27:08 -0400

    first commit

---
+++ firebase-init.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyA2y8j2owXpp1PPTCTcvQgpiYETsPhTFiY",
+
+    authDomain: "controle-de-gastos---av1.firebaseapp.com",
+
+    projectId: "controle-de-gastos---av1",
+
+    storageBucket: "controle-de-gastos---av1.firebasestorage.app",
+
+    messagingSenderId: "645518177609",
+
+    appId: "1:645518177609:web:8738c36f461836ec701698"
+
+    };
+    firebase.initializeApp(firebaseConfig);
+    const db = firebase.firestore();
